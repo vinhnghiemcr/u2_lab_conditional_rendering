afc808aa8ffd6bebffbfb5a397363119b3f3e6e0
Michael Lackey <59581978+mlackey9601@users.noreply.github.com>
2021-02-18 19:37:00 -0600

    Remove imports

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,4 @@
 import React, { Component } from 'react'
-import ErrorPage from './ErrorPage'
-import LandingPage from './LandingPage'
-import Info from './Info'
-import Thanks from './Thanks'
 
 export default class Form extends Component {
   render() {
